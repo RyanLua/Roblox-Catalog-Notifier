28320e21fdac28df9ffff842e63932f10fa6ab2f
Ryan Luu <ryan.luu@ryanluu.dev>
2024-03-18 20:58:16 +0000

    Add tweet queues

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const createTweet = require('./create_tweet');
 const https = require('https');
 
 let previousData = [];
+let tweetQueue = [];
 
 function fetchData() {
     https.get('https://catalog.roblox.com/v2/search/items/details?Category=13&SortType=3&Limit=10', (res) => {
@@ -16,7 +17,7 @@ function fetchData() {
             const newItems = jsonData.data.filter(item => !previousData.some(prevItem => prevItem.id === item.id));
 
             newItems.forEach(item => {
-                createTweet(`New Item: ${item.name} by ${item.creatorName}\nPrice: ${item.price} Robux\n\nhttps://www.roblox.com/catalog/${item.productId.toString()}/`);
+                tweetQueue.push(`New Item: ${item.name} by ${item.creatorName}\nPrice: ${item.price} Robux\n\nhttps://www.roblox.com/catalog/${item.productId.toString()}/`);
             });
 
             previousData = jsonData.data;
@@ -25,6 +26,13 @@ function fetchData() {
         console.log("Error: " + err.message);
     });
 }
-fetchData()
 
+function postTweet() {
+    if (tweetQueue.length > 0) {
+        createTweet(tweetQueue.shift());
+    }
+}
+
+fetchData();
 setInterval(fetchData, 60000); // Fetch data every 60 seconds
+setInterval(postTweet, 10000); // Post a tweet every 10 seconds
