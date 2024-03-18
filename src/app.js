5336e683a2f2cc90ed9b6a054797f734e3b2cdc3
Ryan Luu <ryan.luu@ryanluu.dev>
2024-03-18 20:27:28 +0000

    Add functionality to fetch and tweet new Roblox items

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
 const createTweet = require('./create_tweet');
+const https = require('https');
 
-createTweet('Name: Platinum Plumbob\nBy: Daylight UGC\nPrice: 60 Robux\nhttps://www.roblox.com/catalog/1780167649');
+let previousData = [];
+
+function fetchData() {
+    https.get('https://catalog.roblox.com/v2/search/items/details?SortType=3', (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        res.on('end', () => {
+            const jsonData = JSON.parse(data);
+            const newItems = jsonData.data.filter(item => !previousData.some(prevItem => prevItem.id === item.id));
+
+            newItems.forEach(item => {
+                createTweet(`New Item: ${item.name} by ${item.creatorName}\nPrice: ${item.price} Robux\n\nhttps://www.roblox.com/catalog/${item.productId.toString()}/`);
+            });
+
+            previousData = jsonData.data;
+        });
+    }).on('error', (err) => {
+        console.log("Error: " + err.message);
+    });
+}
+fetchData()
+
+setInterval(fetchData, 60000); // Fetch data every 60 seconds
